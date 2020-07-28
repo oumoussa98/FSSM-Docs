8cb4a3c7330a3563379dd90170199edeea19577c
abdelouahed <doonabdo98@gmail.com>
2020-07-28 21:23:14 +0000

    update router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Upload from '../views/Upload.vue'
+
 
 Vue.use(VueRouter)
 
@@ -9,7 +11,12 @@ Vue.use(VueRouter)
     path: '/',
     name: 'Home',
     component: Home
-  },
+    },
+    {
+      path: '/upload',
+      name: 'Upload',
+      component: Upload
+    },
   {
     path: '/about',
     name: 'About',
