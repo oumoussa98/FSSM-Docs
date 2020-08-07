08d53f6b7e4c3225a5d2e47cd9f73a4559764177
abdelouahed <doonabdo98@gmail.com>
2020-08-07 12:38:22 +0000

    add a dataGetter

---
+++ src/data/DataGetter.js
@@ -0,0 +1,172 @@
+import * as firebase from "firebase"
+import "firebase/storage"
+export default {
+    data: {},
+    succeed: true,
+    setData(smodules) {
+        smodules.forEach(key => {
+            this.data[key] = [
+                {
+                    name: "Cours",
+                    children: [],
+                },
+                {
+                    name: "Travaux diriges (TD)",
+                    children: [],
+                },
+                {
+                    name: "Controles",
+                    children: [],
+                },
+                {
+                    name: "Exercices",
+                    children: [],
+                }
+            ]
+        
+        })
+    },
+
+    async getdata(smodule, key, path) {
+        let vm = this;
+        let storageRef = firebase.storage().ref();
+        let listRef = storageRef.child(path);
+        // Find all the prefixes and items
+        await listRef
+            .listAll()
+            .then(res => {
+                res.prefixes.forEach(folderRef => {
+                    switch (folderRef.name) {
+                        case smodule:
+                            listRef = storageRef.child(folderRef.fullPath);
+                            listRef
+                                .listAll()
+                                .then(res => {
+                                res.prefixes.forEach(folderRef => {
+                                    switch (folderRef.name) {
+                                        case "Cours":
+                                            listRef = storageRef.child(
+                                                folderRef.fullPath
+                                            );
+                                            listRef
+                                                .listAll()
+                                                .then(res => {
+                                                    res.items.forEach(itemRef => {
+                                                        let obj = {};
+                                                        obj.name = itemRef.name;
+                                                        obj.file = itemRef.name.slice(itemRef.name.lastIndexOf(".") + 1);
+                                                        storageRef
+                                                            .child(
+                                                                itemRef.fullPath
+                                                            )
+                                                            .getDownloadURL()
+                                                            .then((url) => {
+                                                                obj.href = url;
+                                                            });
+                                                        vm.data[
+                                                            key
+                                                        ][0].children.push(obj);
+                                                    });
+                                                });
+                                            break;
+                                        case "Travaux diriges (TD)":
+                                            listRef = storageRef.child(
+                                                folderRef.fullPath
+                                            );
+                                            listRef
+                                                .listAll()
+                                                .then(res => {
+                                                    res.items.forEach(itemRef => {
+                                                        let obj = {};
+                                                        obj.name = itemRef.name;
+                                                        obj.file = itemRef.name.slice(itemRef.name.lastIndexOf(".") + 1);
+                                                        storageRef
+                                                            .child(
+                                                                itemRef.fullPath
+                                                            )
+                                                            .getDownloadURL()
+                                                            .then((url) => {
+                                                                obj.href = url;
+                                                            });
+                                                        vm.data[
+                                                            key
+                                                        ][1].children.push(obj);
+                                                    });
+                                                });
+                                            break;
+                                        case "Controles":
+                                            listRef = storageRef.child(
+                                                folderRef.fullPath
+                                            );
+                                            listRef
+                                                .listAll()
+                                                .then(res => {
+                                                    res.items.forEach(itemRef => {
+                                                        let obj = {};
+                                                        obj.name = itemRef.name;
+                                                        obj.file = itemRef.name.slice(itemRef.name.lastIndexOf(".") + 1);
+                                                        storageRef
+                                                            .child(
+                                                                itemRef.fullPath
+                                                            )
+                                                            .getDownloadURL()
+                                                            .then((url) => {
+                                                                obj.href = url;
+                                                            });
+                                                        vm.data[
+                                                            key
+                                                        ][2].children.push(obj);
+                                                    });
+                                                });
+                                            break;
+                                        case "Exercices":
+                                            listRef = storageRef.child(
+                                                folderRef.fullPath
+                                            );
+                                            listRef
+                                                .listAll()
+                                                .then(res => {
+                                                    res.items.forEach(itemRef => {
+                                                        let obj = {};
+                                                        obj.name = itemRef.name;
+                                                        obj.file = itemRef.name.slice(itemRef.name.lastIndexOf(".") + 1);
+                                                        storageRef
+                                                            .child(
+                                                                itemRef.fullPath
+                                                            )
+                                                            .getDownloadURL()
+                                                            .then((url) => {
+                                                                obj.href = url;
+                                                            })
+                                                        vm.data[
+                                                            key
+                                                        ][3].children.push(obj);
+                                                    })
+                                                })
+                                    }
+                                })
+                                })  
+                    }
+                })
+            })
+                .catch(error => {
+                    console.log(error)
+                    vm.succeed = false
+                });
+    },
+    getAllData(smodules, path, slugs) {
+        let vm = this
+            vm.data = {};
+            vm.setData(slugs);
+        let keys = [];
+            Object.keys(vm.data).forEach((key) => {
+            keys.push(key);
+        });
+
+         for (let i = 0; i < smodules.length; i++) {
+             vm.getdata(smodules[i], keys[i],path);
+        }
+    },
+}
+
+
