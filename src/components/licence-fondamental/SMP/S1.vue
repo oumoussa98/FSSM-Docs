dfd8ed128a8f2d484be43f12f8f47c6043762e87
abdelouahed <doonabdo98@gmail.com>
2020-08-07 12:37:14 +0000

    add smp s1...s3 components

---
+++ src/components/licence-fondamental/SMP/S1.vue
@@ -0,0 +1,31 @@
+<template>
+	<ContentDisplayer :smodules="smodules" :slugs="slugs" :path="path">
+	</ContentDisplayer>
+</template>
+
+<script defer>
+export default {
+	// Data =================
+	data: () => ({
+		smodules: [
+			"Mecanique du point",
+			"Thermodynamique 1",
+			"Atomistique",
+			"Thermochimie",
+			"Analyse 1",
+			"Algebre 1",
+			"Langue & terminologie 1",
+		],
+		slugs: [
+			"mecanique_du_point",
+			"thermodynamique",
+			"atomistique",
+			"thermochimie",
+			"analyse",
+			"algebre",
+			"langue_terminologie",
+		],
+		path: "/Licence Fondamental/S1/SMP/",
+	}),
+};
+</script>
---
+++ src/components/licence-fondamental/SMP/S3.vue
@@ -0,0 +1,29 @@
+<template>
+	<ContentDisplayer :smodules="smodules" :slugs="slugs" :path="path">
+	</ContentDisplayer>
+</template>
+
+<script defer>
+export default {
+	// Data =================
+	data: () => ({
+		smodules: [
+			" Mecanique du solide",
+			" Thermodynamique 2",
+			" electromagnetisme dans le vide",
+			" Chimie Organique generale",
+			" Analyse 3",
+			"Analyse numerique et algorithme",
+		],
+		slugs: [
+			"mecanique_du_solide",
+			"thermodynamique_2",
+			"electromagnetisme_dans_le_vide",
+			"chimie_organique_generale",
+			"analyse_3",
+			"analyse_numerique_et_algorithme",
+		],
+		path: "/Licence Fondamental/S3/SMP/",
+	}),
+};
+</script>
