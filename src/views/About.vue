4fc48ad215c030c9ebd9da0327e2be6982a549bb
abdelouahed <doonabdo98@gmail.com>
2020-08-10 17:42:27 +0000

    update about.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,15 @@
 <template>
-	<v-container class="d-flex justify-center mt-6">
-		<h1>Updates comming soon</h1>
+	<v-container class="mt-6">
+		<h1 class="ml-4">About FSSM Documents</h1>
+		<v-col cols="10">
+			<p>
+				This website is made for FSSM (Faculté des Sciences Semlalia
+				Marrakech) students so that they have access to the courses and
+				everything related to like controles, TD, TP and exercises. It's
+				non-profitable, no anoyying ads. Any one could contribute to
+				this website by uploading the files that he was studying with.
+			</p>
+		</v-col>
 	</v-container>
 </template>
 <script>
