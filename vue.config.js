6e558386b0908490eea114dbaf92cbf28e8b0392
abdelouahed <doonabdo98@gmail.com>
2020-08-10 17:56:10 +0000

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,15 +3,20 @@ const PrerenderSPAPlugin = require("prerender-spa-plugin");
 
 module.exports = {
 	transpileDependencies: ["vuetify"],
-	plugins: [
-		new PrerenderSPAPlugin(
-			// Absolute path to compiled SPA
-			path.resolve(__dirname, "dist"),
-			// List of routes to prerender
-			["/contact"],
-			{
-				// options
-			}
-		),
-	],
+	configureWebpack: () => {
+		if (process.env.NODE_ENV !== "production") return;
+		return {
+			plugins: [
+				new PrerenderSPAPlugin(
+					// Absolute path to compiled SPA
+					path.resolve(__dirname, "dist"),
+					// List of routes to prerender
+					["/contact"],
+					{
+						// options
+					}
+				),
+			],
+		};
+	},
 };
