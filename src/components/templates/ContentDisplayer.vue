53e53b4c3769d1614c7f8a7561beef95c55c0e27
abdelouahed <doonabdo98@gmail.com>
2020-08-10 18:51:24 +0000

    update ContentDisplayer.vue

--- src/components/templates/ContentDisplayer.vue
+++ src/components/templates/ContentDisplayer.vue
@@ -41,7 +41,7 @@
 											text
 											color="success"
 											v-if="item.file"
-											:href="item.href"
+											@click="download(item.href)"
 											v-bind="attrs"
 											v-on="on"
 											class="px-0 mx-0"
@@ -99,6 +99,47 @@ export default {
 		isEmpty: (item) => {
 			if (!item.file && item.children.length === 0) return true;
 			return false;
+		},
+		download(item) {
+			let w = "800";
+			let h = "500";
+			// Fixes dual-screen position
+			let dualScreenLeft =
+				window.screenLeft != undefined
+					? window.screenLeft
+					: screen.left;
+			let dualScreenTop =
+				window.screenTop != undefined ? window.screenTop : screen.top;
+
+			let width = window.innerWidth
+				? window.innerWidth
+				: document.documentElement.clientWidth
+				? document.documentElement.clientWidth
+				: screen.width;
+			let height = window.innerHeight
+				? window.innerHeight
+				: document.documentElement.clientHeight
+				? document.documentElement.clientHeight
+				: screen.height;
+
+			let left = width / 2 - w / 2 + dualScreenLeft;
+			let top = height / 2 - h / 2 + dualScreenTop;
+			let newWindow = window.open(
+				item,
+				"download",
+				"scrollbars=yes, width=" +
+					w +
+					", height=" +
+					h +
+					", top=" +
+					top +
+					", left=" +
+					left
+			);
+			newWindow.focus();
+			newWindow.onblur = () => {
+				newWindow.close();
+			};
 		},
 	},
 	// lifecycle hooks ============
