e3e84459c16d2bf04992c0bd438c94e7ac175ead
abdelouahed <doonabdo98@gmail.com>
2020-08-10 17:39:41 +0000

    add prerender-spa-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
+
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+	transpileDependencies: ["vuetify"],
+	plugins: [
+		new PrerenderSPAPlugin(
+			// Absolute path to compiled SPA
+			path.resolve(__dirname, "dist"),
+			// List of routes to prerender
+			["/contact"],
+			{
+				// options
+			}
+		),
+	],
+};
