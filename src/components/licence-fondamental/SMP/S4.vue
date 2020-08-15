589b61b035ccaf8c4b9ea3b39c25af1caab645e7
abdelouahed <doonabdo98@gmail.com>
2020-08-15 20:45:21 +0000

    add S4 and S5 for  SMP

---
+++ src/components/licence-fondamental/SMP/S4.vue
@@ -0,0 +1,29 @@
+<template>
+	<ContentDisplayer :smodules="smodules" :slugs="slugs" :path="path">
+	</ContentDisplayer>
+</template>
+
+<script defer>
+export default {
+	// Data =================
+	data: () => ({
+		smodules: [
+			" Electronique de base",
+			"Optique physique",
+			"Electricite 3",
+			"Mecanique quantique",
+			"Cristallographie geometrique",
+			"Informatique",
+		],
+		slugs: [
+			"electronique_de base",
+			"optique_physique",
+			"electricite_3",
+			"mecanique_quantique",
+			"cristallographie_geometrique",
+			"informatique",
+		],
+		path: "/Licence Fondamental/S4/SMP/",
+	}),
+};
+</script>
---
+++ src/components/licence-fondamental/SMP/S5.vue
@@ -0,0 +1,29 @@
+<template>
+	<ContentDisplayer :smodules="smodules" :slugs="slugs" :path="path">
+	</ContentDisplayer>
+</template>
+
+<script defer>
+export default {
+	// Data =================
+	data: () => ({
+		smodules: [
+			"Electronique analogique",
+			"Mecanique analytique et vibration",
+			"Physique nucleaire",
+			"Physique des materiaux",
+			"Physique quantique",
+			"Physique statistique",
+		],
+		slugs: [
+			"Electronique_analogique",
+			"Mecanique_analytique_et_vibration",
+			"Physique_nucleaire",
+			"Physique_des_materiaux",
+			"Physique_quantique",
+			"Physique_statistique",
+		],
+		path: "/Licence Fondamental/S5/SMP/",
+	}),
+};
+</script>
