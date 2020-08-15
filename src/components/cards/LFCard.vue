27aa4c49d4e2802df36b9dbb08b1d7b505417072
abdelouahed <doonabdo98@gmail.com>
2020-08-15 20:47:15 +0000

    update LFCard.vue add smc

--- src/components/cards/LFCard.vue
+++ src/components/cards/LFCard.vue
@@ -31,8 +31,8 @@
 <script>
 export default {
 	data: () => ({
-		lfSemestres: ["S1", "S2"],
-		lfFilieres: ["SMP"],
+		lfSemestres: ["S1", "S2", "S3", "S4", "S5"],
+		lfFilieres: ["SMP", "SMC"],
 		semestre: "",
 		filiere: "",
 		path: "",
