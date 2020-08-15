eff1678b6f1d72f28dc2a48ef8407cc176d8e1e5
abdelouahed <doonabdo98@gmail.com>
2020-08-15 22:13:03 +0000

    update main.js

--- src/main.js
+++ src/main.js
@@ -3,25 +3,25 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import firebase from "firebase/app";
-import "firebase/storage"
-
+import "firebase/storage"	
 Vue.component(
 	'ContentDisplayer', () => import('@/components/templates/ContentDisplayer')
 )
-
 Vue.config.productionTip = false;
-
-new Vue({
-	router,
-	vuetify,
-	render: (h) => h(App),
-	created() {
-		firebase.initializeApp({
-			apiKey: "AIzaSyBpjbXVMUKHx77navIKO8IChlql94oFJmQ",
-			authDomain: "fssm-fb8e7.firebaseapp.com",
-			databaseURL: "https://fssm-fb8e7.firebaseio.com",
-			projectId: "fssm-fb8e7",
-			storageBucket: "fssm-fb8e7.appspot.com",
-		});
-	},
-}).$mount("#app");
+const root = new Vue({
+		router,
+		vuetify,
+		render: (h) => h(App),
+		created() {
+			firebase.initializeApp({
+				apiKey: "AIzaSyBpjbXVMUKHx77navIKO8IChlql94oFJmQ",
+				authDomain: "fssm-fb8e7.firebaseapp.com",
+				databaseURL: "https://fssm-fb8e7.firebaseio.com",
+				projectId: "fssm-fb8e7",
+				storageBucket: "fssm-fb8e7.appspot.com",
+			});
+		},
+})
+document.addEventListener('DOMContentLoaded', function () {
+	root.$mount('#app')
+})
